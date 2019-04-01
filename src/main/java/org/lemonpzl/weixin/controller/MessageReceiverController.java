98960f315fd1406aa30e1f6ddd59e5270d24059d
ASUS <ASUS@DESKTOP-CSDVIVD>
2019-04-01 22:49:12 +0800

    接受微信公众号平台发送给我们的信息

--- src/main/java/org/lemonpzl/weixin/controller/MessageReceiverController.java
+++ src/main/java/org/lemonpzl/weixin/controller/MessageReceiverController.java
@@ -1,5 +1,9 @@
 package org.lemonpzl.weixin.controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 // @RequestMapping表示的含义：URL跟控制器的关系映射
 @RequestMapping("/pzl/weixin/receiver")
 public class MessageReceiverController {
+	// 日志记录器
+	private static final Logger LOG = LoggerFactory.getLogger(MessageReceiverController.class);
 
 	@GetMapping // 只处理GET请求
 	public String echo(//
@@ -28,5 +34,22 @@ public class MessageReceiverController {
 
 		// 原路返回echostr的值，返回以后微信公众号平台就能够认为：服务器对接成功
 		return echostr;
+	}
+	@PostMapping
+	// @RequestBody注解表示把请求内容获取出来，并且转换为String传入给xml参数
+	public String onMessage(//
+			@RequestParam("signature") String signature, //
+			@RequestParam("timestamp") String timestamp, //
+			@RequestParam("nonce") String nonce, //
+			@RequestBody String xml) {
+		// 收到消息
+		// {}是占位符，第一个{}会把第二个参数的值自动填入
+		// LOG.trace必须要求日志记录器的配置为trace级别才能输出
+		LOG.trace("收到的消息原文：\n{}\n------------------------------", xml);
+		// 转换消息
+		// 把消息丢入队列
+		// 消费队列中的消息
+		// 产生客服消息
+		return "success";
 	}
 }
