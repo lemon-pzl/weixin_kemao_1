f3fe7d875337b6f54df8f34aaf31c338f676942f
ASUS <ASUS@DESKTOP-CSDVIVD>
2019-04-05 20:50:41 +0800

    palpalp

--- src/main/java/org/lemonpzl/weixin/controller/MessageReceiverController.java
+++ src/main/java/org/lemonpzl/weixin/controller/MessageReceiverController.java
@@ -1,4 +1,4 @@
-package org.fkjava.weixin.controller;
+package org.lemonpzl.weixin.controller;
 
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectOutputStream;
@@ -23,16 +23,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-// 控制器 : 负责接收用户的请求参数、调用业务逻辑层代码、返回视图/结果给客户端（浏览器）
-// @Controller  基于JSP的控制器
-// @RestController 符合RESTful风格的WEB服务的控制器
-// RESTful通过不同的请求方法调用不同的处理程序，返回的结果仅仅是数据，不包含视图（HTML、JSP）
 @RestController
-// 各自写代码的时候，把/kemao_1改为【/拼音名】，用于后面作为路径反向代理的时候区分不同人的代码
-// @RequestMapping表示的含义：URL跟控制器的关系映射
+//各自写代码的时候，把/kemao_1改为【/拼音名】，用于后面作为路径反向代理的时候区分不同人的代码
+//@RequestMapping表示的含义：URL跟控制器的关系映射
 @RequestMapping("/pzl/weixin/receiver")
 public class MessageReceiverController {
-
+	
+	
+	
 	private static final Logger LOG = LoggerFactory.getLogger(MessageReceiverController.class);
 
 	@Autowired
@@ -116,4 +114,5 @@ public class MessageReceiverController {
 		// 由于后面会把消息放入队列中，所以这里直接返回success。
 		return "success";
 	}
+
 }
